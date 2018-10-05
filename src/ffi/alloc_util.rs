b1b78478102f4f8a8819055d42f6f725bca5fed3
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2018-10-05 01:25:52 -0700

    1.12 support

--- src/ffi/alloc_util.rs
+++ src/ffi/alloc_util.rs
@@ -107,7 +107,7 @@ impl<Ty:Sized+Default+Clone> alloc::Allocator<Ty> for SubclassableAllocator<Ty> 
 
 
 #[cfg(feature="no-stdlib")]
-static mut G_SLICE:&mut[u8] = &mut[];
+static mut G_SLICE:&'static mut[u8] = &mut[];
 #[cfg(feature="no-stdlib")]
 #[derive(Debug)]
 pub struct MemoryBlock<Ty:Sized+Default>(*mut[Ty]);
